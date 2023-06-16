63481f9007b94b954cfdd7d8b1e691943df04742
saiidhanna21 <hanna.saiid2003@gmail.com>
2023-06-16 13:04:45 +0300

    Init

---
+++ app.py
@@ -0,0 +1,168 @@
+import base64
+import datetime
+from io import BytesIO
+import json
+import os
+from PIL import Image
+from flask import Flask, jsonify, render_template, request, send_from_directory, session
+import mysql.connector
+from flask_cors import CORS
+from cryptography.fernet import Fernet
+
+app = Flask(__name__)
+CORS(app) 
+app.secret_key = 'secret'
+
+@app.route('/')
+def index():
+    try:
+        session_time_str = request.args.get('session_time', default=None)
+
+        if session_time_str:
+            session_time = datetime.datetime.strptime(
+                session_time_str, "%Y-%m-%d %H:%M:%S")
+            #Set the session variable with the provided session time
+            session['session_time'] = session_time
+        else:
+            raise ValueError("Unauthorized Access")
+    except:
+        return jsonify({'message':'Unauthorized Access'})
+    return render_template('index.html')
+
+
+@app.route('/data')
+def get_data():
+    # Connect to MySQL
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='',
+        database='students'
+    )
+
+    # Execute a query
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM students')
+    data = cursor.fetchall()
+
+    # Close the connection
+    cursor.close()
+    conn.close()
+    result = []
+
+    for item in data:
+        # Convert bytes to base64 string
+        blob_base64 = base64.b64encode(item[3]).decode('utf-8')
+        key = b'FwMO2y7T1iWUEhmaw3iVHn56jE09wjTMhrvBbUIoTLQ='
+        cipher_suite = Fernet(key)
+        # Create a dictionary with the converted blob
+        item_dict = {
+            'id': item[0],
+            'name': cipher_suite.decrypt(item[1]).decode(),
+            'tpNumber': cipher_suite.decrypt(item[2]).decode(),
+            'image': blob_base64
+        }
+        
+        result.append(item_dict)
+
+        # Serialize the result list to JSON
+    json_data = json.dumps(result)
+
+    return json_data
+    
+@app.route('/api/save_image', methods=['POST'])
+def save_image():
+    # Retrieve the base64 image data from the request payload
+    base64_image = request.json.get('base64_image')
+    index = request.json.get('index')
+    if base64_image:
+        # Decode the base64 image data
+        image_data = base64.b64decode(base64_image)
+
+        # Create a PIL Image object
+        image = Image.open(BytesIO(image_data))
+
+        # Define the file path and name
+        file_path = f"static/labels/{index}.png"
+
+        # Create the required directories if they don't exist
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+        # Save the image as a PNG file, overwriting if it exists
+        image.save(file_path, "PNG", overwrite=True)
+
+        # Return the file path as the API response
+        return jsonify({"file_path": file_path})
+
+    # Return an error response if base64_image parameter is missing
+    return jsonify({"error": "Missing base64_image parameter"}), 400
+
+
+@app.route('/api/get_student_id', methods=['POST'])
+def get_student_id():
+    key = b'FwMO2y7T1iWUEhmaw3iVHn56jE09wjTMhrvBbUIoTLQ='
+    cipher_suite = Fernet(key)
+    person_name = request.json.get('personName')
+    
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='',
+        database='students'
+    )
+    
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, name FROM students")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        # Assuming you have a decrypt_name function
+        decrypted_name = cipher_suite.decrypt(row[1]).decode()
+        if decrypted_name == person_name:
+            person_id = row[0]
+            break
+
+    cursor.close()
+    conn.close()
+    return jsonify({'student_id': person_id})
+
+@app.route('/api/update_attendance',methods=['POST'])
+def update_attendance():
+    student_id = request.json.get('student_id')
+    otp_input = request.json.get('otp_input')
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='',
+        database='students'
+    )
+
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT otp FROM courses LIMIT 1")
+    otp_in_db = cursor.fetchone()[0]
+    if otp_input == otp_in_db:    
+        cursor.execute(
+            "UPDATE attendance SET status = 'Attended' WHERE student_id = %s AND session_time = %s",
+            (student_id, session['session_time']))
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        return jsonify({'message': 'Attendance updated successfully'})
+    else:
+        return jsonify({'message': 'OTP verification failed'})
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'static'), filename)
+
+if __name__ == '__main__':
+    CORS(app)
+    app.run(debug=True,host='0.0.0.0')
+
---
+++ static/script.js
@@ -0,0 +1,186 @@
+const video = document.getElementById('video');
+var data;
+let attendanceUpdated = false;
+async function getData() {
+	const response = await fetch('http://localhost:5000/data');
+	data = await response.json();
+}
+getData()
+Promise.all([
+	faceapi.nets.ssdMobilenetv1.loadFromUri('/static/models'),
+	faceapi.nets.faceRecognitionNet.loadFromUri('/static/models'),
+	faceapi.nets.faceLandmark68Net.loadFromUri('/static/models'),
+]).then(startWebcam);
+
+function startWebcam() {
+	navigator.mediaDevices
+		.getUserMedia({
+			video: true,
+			audio: false,
+		})
+		.then((stream) => {
+			video.srcObject = stream;
+		})
+		.catch((error) => {
+			console.error(error);
+		});
+}
+
+async function getLabeledFaceDescriptions() {
+	try {
+		const data_values = await Promise.all(
+			data.map(async (item,index) => {
+				const label = item.name;
+				var filePath;
+				const descriptions = [];
+				for (let i = 1; i <= 1; i++) {
+					const imageData = item.image;
+					await fetch('/api/save_image', {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						body: JSON.stringify({
+							base64_image: imageData,
+							index
+						}),
+					})
+						.then((response) => response.json())
+						.then((data) => {
+							// Retrieve the file path from the API response
+							filePath = data.file_path;
+						})
+						.catch((error) => console.error(error));
+					
+					filePath = `/${filePath}`
+					const img = await faceapi.fetchImage(filePath);
+					const detections = await faceapi
+						.detectSingleFace(img)
+						.withFaceLandmarks()
+						.withFaceDescriptor();
+					if (detections) {
+						descriptions.push(detections.descriptor);
+					} else {
+						console.log('No face detected in the image:', filePath);
+					}
+				}
+				return new faceapi.LabeledFaceDescriptors(label, descriptions);
+			})
+		);
+
+		return data_values;
+	} catch (error) {
+		console.error('Error:', error);
+	}
+}
+
+
+
+video.addEventListener('play', async () => {
+	const labeledFaceDescriptors = await getLabeledFaceDescriptions();
+	const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
+
+	const canvas = faceapi.createCanvasFromMedia(video);
+	document.body.append(canvas);
+
+	const displaySize = { width: video.width, height: video.height };
+	faceapi.matchDimensions(canvas, displaySize);
+
+	setInterval(async () => {
+		const detections = await faceapi
+			.detectAllFaces(video)
+			.withFaceLandmarks()
+			.withFaceDescriptors();
+
+		const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+		canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+		const results = resizedDetections.map((d) => {
+			return faceMatcher.findBestMatch(d.descriptor);
+		});
+		results.forEach((result, i) => {
+			const box = resizedDetections[i].detection.box;
+			const drawBox = new faceapi.draw.DrawBox(box, {
+				label: result,
+			});
+			drawBox.draw(canvas);
+
+			if (result.label !== 'unknown' && !attendanceUpdated) {
+				const personName = result.label;
+				updateAttendance(personName);
+			}
+		});
+	}, 100);
+});
+async function updateAttendance(personName) {
+	
+	if (!attendanceUpdated) {
+		attendanceUpdated = true;
+		await fetch('/api/get_student_id', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				personName: personName,
+			}),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				const student_id = data.student_id;
+
+				const otpHeader = document.createElement('h3')
+				otpHeader.innerText = "Enter Otp: "
+				// Display the OTP input field and submit button
+				const otpInput = document.createElement('input');
+				otpInput.setAttribute('type', 'text');
+				otpInput.setAttribute('id', 'otp_input');
+				otpInput.setAttribute('placeholder', 'Enter OTP');
+
+				const submitButton = document.createElement('button');
+				submitButton.setAttribute('type', 'submit');
+				submitButton.innerText = 'Submit';
+
+				document.body.appendChild(otpHeader);
+				document.body.appendChild(otpInput);
+				document.body.appendChild(submitButton);
+				const videoElement = document.getElementById('video');
+
+				videoElement.parentNode.insertBefore(
+					otpHeader,
+					videoElement.nextSibling
+				);
+				videoElement.parentNode.insertBefore(otpInput, otpHeader.nextSibling);
+				videoElement.parentNode.insertBefore(
+					submitButton,
+					otpInput.nextSibling
+				);
+
+
+				// Add an event listener to the submit button
+				submitButton.addEventListener('click', async () => {
+					const otp_input = otpInput.value;
+					
+					await fetch('/api/update_attendance', {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						body: JSON.stringify({ student_id: student_id, otp_input: otp_input }),
+					})
+						.then((response) => response.json())
+						.then((data) => {
+							if (data.message === 'Attendance updated successfully') {
+								// Handle successful attendance update
+								document.getElementById('message').innerText = "Attendance Updated"
+							} else {
+								// Handle OTP verification failure
+								document.getElementById('message').innerText =
+									'OTP verification failed';
+							}
+						});
+				});
+			})
+	}
+}
